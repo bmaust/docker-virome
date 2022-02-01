0d47f5d8aa46de17cb72e3ef7076cab5a33f8e50
Brandon Maust <bmaust@Yavin>
2022-01-31 16:29:10 -0800

    fix miniconda install

--- virome-base/base-apps.sh
+++ virome-base/base-apps.sh
@@ -3,9 +3,10 @@
 
 apt-get -y update
 apt-get -y -q install wget git build-essential zlib1g-dev
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash ~/miniconda.sh -b -p /opt/miniconda
-source .bashrc
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
+bash /tmp/miniconda.sh -b -p /opt/miniconda
+rm /tmp/miniconda
+#source .bashrc
 apt-get -y -q install python3-biopython hmmer prodigal python3-pandas python3-matplotlib python3-seaborn python3-numpy ncbi-blast+
 apt-get clean
 rm -rf /var/lib/apt/lists/*
