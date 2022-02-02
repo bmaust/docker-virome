dc02803534048097b797a53724b94cb66c7cfdb2
Brandon Maust <bmaust@Yavin>
2022-02-01 18:22:50 -0800

    prune unnecessary libraries and clean up after apt and conda, reduces image size

--- virome-base/base-apps.sh
+++ virome-base/base-apps.sh
@@ -2,11 +2,17 @@
 # runs as root on install to set up base tools on top of fresh OS
 
 apt-get -y update
-apt-get -y -q install wget git build-essential zlib1g-dev
+apt-get -y -q install wget git 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
 bash /tmp/miniconda.sh -b -p /opt/miniconda
-rm /tmp/miniconda
-#source .bashrc
-apt-get -y -q install python3-biopython hmmer prodigal python3-pandas python3-matplotlib python3-seaborn python3-numpy ncbi-blast+
+rm /tmp/miniconda.sh
+export PATH=/opt/miniconda/bin:$PATH
+conda init bash
+source /root/.bashrc
+conda update -y -q -n base -c defaults conda
+
+#cleanup
 apt-get clean
 rm -rf /var/lib/apt/lists/*
+
+conda clean -a -q
